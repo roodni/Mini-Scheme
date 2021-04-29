973ac05a815266ce12daea409e42f4ca31be2154
rodoni <41773436+roodni@users.noreply.github.com>
2021-04-29 17:22:38 +0900

    ビルトイン手続きの呼び出し

--- mini.scm
+++ mini.scm
@@ -1,14 +1,36 @@
 ;;; util
+
+(define (tag id value)
+  (cons id value))
+(define (tagged? id obj)
+  (and (pair? obj) (eq? id (car obj))))
+(define (untag tagged)
+  (cdr tagged))
+
+(define (prefixed-list? prefix len obj)
+  (and
+    (list? obj)
+    (= len (length obj))
+    (eq? prefix (car obj))))
+
 (define (match? pat obj)
   (cond
     ((eq? pat '_) #t)
     ((eq? pat 'symbol) (symbol? obj))
+    ((eq? pat 'string) (string? obj))
     ((null? pat) (null? obj))
-    ((and
-        (list? pat)
-        (eq? (car pat) 'quote)
-        (= (length pat) 2))
+    ((prefixed-list? 'quote 2 pat)
       (equal? (cadr pat) obj))
+    ((prefixed-list? '* 3 pat)
+      (let
+        ( (pat-loop (cadr pat))
+          (pat-end (caddr pat)))
+        (or
+          (match? pat-end obj)
+          (and
+            (pair? obj)
+            (match? pat-loop (car obj))
+            (match? pat (cdr obj))))))
     ((pair? pat)
       (and
         (pair? obj)
@@ -16,37 +38,129 @@
         (match? (cdr pat) (cdr obj))))
     (else (error "match?: invalid pattern:" pat))))
 
-;;; env
-(define mini-env-empty '())
 
-(define (mini-env-lookup var env)
+
+;;; error
+(define mini-error-tag 'mini-error)
+
+(define (raise-mini-error msg expr)
+  (raise (tag mini-error-tag (list msg expr))))
+
+(define (mini-error? obj)
+  (and
+    (tagged? mini-error-tag obj)
+    (match? '(_ _) (untag obj))))
+
+(define (mini-error.msg err)
+  (car (untag err)))
+
+(define (mini-error.expr err)
+  (cadr (untag err)))
+
+
+;;; value
+(define v-builtin-tag 'builtin)
+(define (v-builtin-tagged name argn-min variadic proc)
+  (tag v-builtin-tag (list name argn-min variadic proc)))
+(define (v-builtin.name builtin) (car builtin))
+(define (v-builtin.argn-min builtin) (cadr builtin))
+(define (v-builtin.variadic? builtin) (caddr builtin))
+(define (v-builtin.proc builtin) (cadddr builtin))
+
+(define v-lambda-tag 'lambda)
+(define v-pair-tag 'pair)
+
+
+
+;;; env
+(define (env-bind var value) (cons var value))
+
+(define (env-bind.value bind) (cdr bind))
+
+(define (env-lookup var env)
   (assoc var env))
 
-(define (mini-env-bind-value bind) (cdr bind))
-
-(define (mini-env-extend var value env)
+(define (env-extend var value env)
   (cons (cons var value) env))
 
-(define (mini-env-define var value env)
-  (let ((bind (mini-env-lookup var env)))
+(define (env-define var value env)
+  (let ((bind (env-lookup var env)))
     (if bind
       (begin
         (set-cdr! bind value)
         env)
-      (mini-env-extend var value env))))
+      (env-extend var value env))))
+
+(define env-empty '())
+
+(define (env-init)
+  (define (env-bind-builtin var argn-min variadic proc)
+    (cons
+      var
+      (v-builtin-tagged
+        (symbol->string var)
+        argn-min variadic proc)))
+  (list
+    (env-bind-builtin 'cons 2 #f
+      (lambda (args)
+        (define a (car args))
+        (define d (cadr args))
+        (tag v-pair-tag (cons a d))))
+    (env-bind-builtin '+ 0 #t
+      (lambda (args)
+        (fold
+          (lambda (v total)
+            (if (number? v) (+ v total) (error "not a number")))
+          0 args)))
+  ))
+
 
 ;;; eval
 (define (mini-eval-expr expr top-env env)
   (cond
-    ((symbol? expr)
-      (cond
-        ((mini-env-lookup expr env) => mini-env-bind-value)
-        ((mini-env-lookup expr top-env) => mini-env-bind-value)
-        (else (error "unknown location:" expr))))
     ((number? expr) expr)
     ((boolean? expr) expr)
     ((string? expr) expr)
-    (else (error "syntax error:" expr))))
+    ((null? expr) expr)
+    ((symbol? expr)
+      (cond
+        ((env-lookup expr env) => env-bind.value)
+        ((env-lookup expr top-env) => env-bind.value)
+        (else (raise-mini-error "unknown location:" expr))))
+    ((list? expr) ; 空リストは上で捕捉されるので空リストでない
+      (let*
+        ( (values
+            (map
+              (lambda (e) (mini-eval-expr e top-env env))
+              expr))
+          (callee (car values))
+          (args (cdr values))
+          (argn (length args)))
+        (cond
+          ((tagged? v-builtin-tag callee)
+            (let*
+              ( (builtin (untag callee))
+                (name (v-builtin.name builtin))
+                (argn-min (v-builtin.argn-min builtin))
+                (variadic (v-builtin.variadic? builtin))
+                (proc (v-builtin.proc builtin)))
+              (if
+                (or
+                  (= argn-min argn)
+                  (and variadic (< argn-min argn)))
+                (proc args); あとでエラー処理を書く
+                (raise-mini-error
+                  (string-append
+                    "wrong number of arguments: "
+                    name "requires"
+                    (number->string argn-min) ", but got"
+                    (number->string argn))
+                  expr))))
+          (else (raise-mini-error "invalid application:" expr)))
+      )
+    )
+    (else (raise-mini-error "syntax error:" expr))))
+
 
 ;; returns (value top-env)
 (define (mini-eval-toplevel toplevel top-env)
@@ -55,33 +169,40 @@
       (let*
         ( (var (cadr toplevel))
           (expr (caddr toplevel))
-          (value (mini-eval-expr expr mini-env-empty top-env)))
-        (list '() (mini-env-define var value top-env))))
+          (value (mini-eval-expr expr env-empty top-env)))
+        (list '() (env-define var value top-env))))
     (else
-      (let ((value (mini-eval-expr toplevel top-env mini-env-empty)))
+      (let ((value (mini-eval-expr toplevel top-env env-empty)))
         (list value top-env)))))
 
 
 ;;; repl
 (define (mini-repl)
   (display "mini-scheme intepreter\n")
-  (let loop ((top-env mini-env-empty))
+  (let loop ((top-env (env-init)))
     (display "\n> ")
     (flush)
     (let ((toplevel (read)))
       (if (not (eof-object? toplevel))
         (let*
           ( (res
-              (guard (condition
+              (guard
+                (condition
+                  ((mini-error? condition)
+                    (display (mini-error.msg condition))
+                    (display " ")
+                    (write (mini-error.expr condition))
+                    'error)
                   (else
                     (display "fatal error: ")
-                    (display condition)
-                    'error))
-                (list 'ok (mini-eval-toplevel toplevel top-env)))))
+                    (write condition)
+                    'error)
+                )
+                (tag 'ok (mini-eval-toplevel toplevel top-env)))))
           (cond
-            ((match? '('ok _) res)
+            ((tagged? 'ok res)
               (let*
-                ( (res (cadr res))
+                ( (res (untag res))
                   (value (car res))
                   (top-env (cadr res)))
                 (write value)
